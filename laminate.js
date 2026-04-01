552233471c5c6c9589f1ccc69cc8b27aea07bf72
CeeLazzy <chriscolinlazarus@gmail.com>
2026-04-01 09:43:45 +0200

    Shift images

--- laminate.js
+++ laminate.js
@@ -588,7 +588,7 @@ for (let j = 1; j <= 5; j++) {
 
       // Place image in column 2 only (ExcelJS is zero-indexed for coordinates)
 sheet.addImage(imageId, {
-  tl: { col: 1.2, row: excelRow.number - 0 }, // ✅ shifts image into center
+  tl: { col: 1.2, row: excelRow.number - 1 }, // ✅ shifts image into center
   ext: { width: 80, height: 80 }
 });
     } catch (err) {
